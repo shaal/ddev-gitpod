2bf6e5f2ae516fe1195d7d2782c249b26330d7c6
Ofer Shaal <shaal@palantir.net>
2020-10-27 10:24:18 -0400

    Initial drupal9 installation + ddev

---
+++ web/sites/default/settings.php
@@ -0,0 +1,6 @@
+<?php
+
+// #ddev-generated: Automatically generated Drupal settings file.
+if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
+  include $app_root . '/' . $site_path . '/settings.ddev.php';
+}
