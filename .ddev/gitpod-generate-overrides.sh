4ad93a8e377ab33ecf21906cf9222a921b5ad66b
Randy Fay <randy@randyfay.com>
2021-02-21 08:17:23 -0700

    Refactor startup script

--- .ddev/gitpod-generate-overrides.sh
+++
@@ -1,3 +0,0 @@
-echo 'router_http_port: "8080"' > .ddev/config.gitpod-overrides.yaml
-echo 'router_https_port: "8443"' >> .ddev/config.gitpod-overrides.yaml
-echo 'use_dns_when_possible: false' >> .ddev/config.gitpod-overrides.yaml
--- .ddev/gitpod-setup-ddev.sh
+++ .ddev/gitpod-setup-ddev.sh
@@ -1,16 +1,45 @@
-ddev poweroff
-# Remove docker containers and specific docker images, because of a gitpod bug
+#!/usr/bin/env bash
+
+# Remove docker containers and specific docker images,
+# because of gitpod bug: https://github.com/gitpod-io/gitpod/issues/3174
 docker rm -f $(docker ps -aq) || true
 images_found=$(docker images | awk '/^drud\/ddev-(webserver|ssh-agent|dbserver)/ { print $3 }') || true
 if [[ "$images_found" ]]; then
     docker rmi -f $images_found
 fi
 
+# Generate a config.gitpod.yaml that adds the gitpod
+# proxied ports so they're known to ddev.
+shortgpurl="${GITPOD_WORKSPACE_URL#'https://'}"
+
+cat >>CONFIGEND > .ddev/config.gitpod.yaml
+#ddev-gitpod-generated
+router_http_port: 8080
+router_https:port: 8443
+use_dns_when_possible: false
+
+additional_fqdns:
+- 8080-${shortgpurl}
+- 8025-${shortgpurl}
+- 8036-${shortgpurl}
+CONFIGEND
+
+# We need host.docker.internal inside the container,
+# So add it via docker-compose.xdebug-ip.yaml
+hostip=$(awk "\$2 == \"$HOSTNAME\" { print \$1; }" /etc/hosts)
+cat <<COMPOSEEND >.ddev/docker-compose.xdebug-ip.yaml
+#ddev-gitpod-generated
+version: "3.6"
+services:
+  web:
+    extra_hosts:
+    - "host.docker.internal:${hostip}"
+COMPOSEEND
+
+# Misc housekeeping before start
 mkcert -install
 ddev config global --instrumentation-opt-in=true --router-bind-all-interfaces=true
-.ddev/gitpod-generate-overrides.sh
-.ddev/gitpod-generate-fqdns.sh
-.ddev/gitpod-generate-xdebug-host-ip.sh
+
 ddev start
 echo Your website is available at this url:
 echo $(gp url 8080)
