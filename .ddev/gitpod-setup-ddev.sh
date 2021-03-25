40acbf05e5228dd6b2b48afc0df5ff391cdb7b1f
Ofer Shaal <oshaal@phase2technology.com>
2021-03-25 15:19:32 -0400

    Gitpod bug #3174 got resolved, no need to remove images after ws restart

--- .ddev/gitpod-setup-ddev.sh
+++ .ddev/gitpod-setup-ddev.sh
@@ -6,18 +6,6 @@ set -eu -o pipefail
 
 MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# Remove docker containers and specific docker images,
-# because of gitpod bug: https://github.com/gitpod-io/gitpod/issues/3174
-containers_found="$(docker ps -aq)"
-if [[ ! -z "$containers_found" ]]; then
-    docker rm -f $containers_found || true
-fi
-
-images_found="$(docker images | awk '/^drud\/ddev-(webserver|ssh-agent|dbserver)/ { print $3 }')"
-if [[ ! -z "$images_found" ]]; then
-    docker rmi -f $images_found
-fi
-
 # Generate a config.gitpod.yaml that adds the gitpod
 # proxied ports so they're known to ddev.
 shortgpurl="${GITPOD_WORKSPACE_URL#'https://'}"
