7290d17af506ceb4d6fb4bfe6b12173b0577498d
Randy Fay <randy@randyfay.com>
2021-06-10 12:29:22 -0600

    Stop using router ports and use webserver port instead

--- .ddev/gitpod-setup-ddev.sh
+++ .ddev/gitpod-setup-ddev.sh
@@ -10,18 +10,6 @@ MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # proxied ports so they're known to ddev.
 shortgpurl="${GITPOD_WORKSPACE_URL#'https://'}"
 
-cat <<CONFIGEND > ${MYDIR}/config.gitpod.yaml
-#ddev-gitpod-generated
-router_http_port: 8080
-router_https_port: 8443
-use_dns_when_possible: false
-
-additional_fqdns:
-- 8080-${shortgpurl}
-- 8025-${shortgpurl}
-- 8036-${shortgpurl}
-CONFIGEND
-
 # We need host.docker.internal inside the container,
 # So add it via docker-compose.host-docker-internal.yaml
 hostip=$(awk "\$2 == \"$HOSTNAME\" { print \$1; }" /etc/hosts)
@@ -35,7 +23,4 @@ services:
     - "host.docker.internal:${hostip}"
 COMPOSEEND
 
-# Misc housekeeping before start
-ddev config global --instrumentation-opt-in=true --router-bind-all-interfaces=true
-
-ddev start
+ddev start -y
