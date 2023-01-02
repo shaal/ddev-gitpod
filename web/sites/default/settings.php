b287a1c0381af96e7febc85052840f6d8b73ed6e
imrodmartin <rod.m@navigatetomorrow.com>
2023-01-02 11:57:57 -0500

    Update settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -4,4 +4,5 @@
 if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
   include $app_root . '/' . $site_path . '/settings.ddev.php';
 }
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/default/services.yml';
 $config['system.logging']['error_level'] = 'verbose';
