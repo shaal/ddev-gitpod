658316c4be1b125be834445d45620a9ee6a7f113
imrodmartin <rod.m@navigatetomorrow.com>
2023-01-02 11:46:55 -0500

    Update settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -4,3 +4,4 @@
 if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
   include $app_root . '/' . $site_path . '/settings.ddev.php';
 }
+$config['system.logging']['error_level'] = 'verbose';
